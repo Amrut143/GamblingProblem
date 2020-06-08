015e5d89d6024dccb088ec8ef99e221c9a9ee6ed
Amrut <amrutpanda7789@gmail.com>
2020-06-08 14:45:44 +0530

    win or loss is added

--- Gambling.sh
+++ Gambling.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -x
 
 #@Author: Amrut
-#Gampling Use Case 1
-
-echo "Welcome to Gambling"
+#Gampling Use Case 2
 
 STARTING_STAKE=100
 BET=1
+DAY=1
+WIN=0
+LOSS=1
 
-echo "Starting Stake $STARTING_STAKE"
+echo "Welcome to Gambling"
+
+function gamble()
+{
+	ran=$(( RANDOM%2 ))
+
+	if [ $ran -eq 0 ]
+	then
+		STARTING_STAKE=$(( STARTING_STAKE+BET ))
+		echo "WIN"
+	else
+		STARTING_STAKE=$(( STARTING_STAKE-BET ))
+		echo "LOSS"
+	fi
+}
+
+gamble
+
+echo $STARTING_STAKE
+
