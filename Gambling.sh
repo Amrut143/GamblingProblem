357c1692bd77cc96552b90586bfa2769b9318be6
Amrut <amrutpanda7789@gmail.com>
2020-06-08 15:45:28 +0530

    gambler resign is added

--- Gambling.sh
+++ Gambling.sh
@@ -1,31 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 #@Author: Amrut
-#Gampling Use Case 2
+#Gambling Use Case 3
 
 STARTING_STAKE=100
 BET=1
 DAY=1
 WIN=0
 LOSS=1
+PercentStake=$(( 50*$STARTING_STAKE/100 ))
+MaxWin=$(( $STARTING_STAKE + $PercentStake ))
+MaxLoss=$(( $STARTING_STAKE - $PercentStake ))
 
 echo "Welcome to Gambling"
 
 function gamble()
 {
-	ran=$(( RANDOM%2 ))
+	while [ $STARTING_STAKE -lt $MaxWin ] && [ $STARTING_STAKE -gt $MaxLoss ]
+	do
+		ran=$(( RANDOM%2 ))
 
 	if [ $ran -eq 0 ]
 	then
 		STARTING_STAKE=$(( STARTING_STAKE+BET ))
 		echo "WIN"
+		echo $STARTING_STAKE
 	else
 		STARTING_STAKE=$(( STARTING_STAKE-BET ))
 		echo "LOSS"
+		echo $STARTING_STAKE
 	fi
+	done
 }
 
 gamble
 
-echo $STARTING_STAKE
+echo "Gambler reached to required 50% and resign for the day"
 
