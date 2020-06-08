5f588578eed8de380d9cbeba0e490b7a64b5a42b
Amrut <amrutpanda7789@gmail.com>
2020-06-08 19:11:20 +0530

    Each day won or loss for a month is added

--- Gambling.sh
+++ Gambling.sh
@@ -8,33 +8,53 @@ BET=1
 DAY=1
 WIN=0
 LOSS=1
-stake=$STARTING_STAKE
+
 PercentStake=$(( 50*$STARTING_STAKE/100 ))
 MaxWin=$(( $STARTING_STAKE + $PercentStake ))
 MaxLoss=$(( $STARTING_STAKE - $PercentStake ))
+numOfDays=30
+totalWinOrLoss=0
+winDays=0
+lossDays=0
+
+declare -A eachDay
 
 echo "Welcome to Gambling"
 
 function gamble()
 {
+	stake=$STARTING_STAKE
 	while [ $stake -lt $MaxWin ] && [ $stake -gt $MaxLoss ]
 	do
 		ran=$(( RANDOM%2 ))
 
 	if [ $ran -eq 0 ]
 	then
-		stake=$(( stake+BET ))
+		stake=$(( $stake+$BET ))
 		echo "WIN"
 		echo $stake
 	else
-		stake=$(( stake-BET ))
+		stake=$(( $stake-$BET ))
 		echo "LOSS"
 		echo $stake
 	fi
 	done
 }
 
-gamble
-
-echo "Gambler reached to 50% and resign for the day"
-
+	for (( day=1; day<=$numOfDays; day++ ))
+	do
+		gamble
+				if [ $stake -eq $MaxLoss ]
+				then
+						totalWinOrLoss=$(( $totalWinOrLoss-$PercentStake ))
+						eachDay["Day $day"]=-$PercentStake
+						(( lossDays++ ))
+				else
+						totalWinOrLoss=$(( $totalWinOrLoss+$PercentStake ))
+						eachDay["Day $day"]=$PercentStake
+						(( winDays++ ))
+				fi
+	done
+echo "Number of days Win is $winDays by $(($winDays*$PercentStake))" 
+echo "Number of days Loss is $lossDays by  $(($lossDays*$PercentStake))"
+echo "Total amount of Win Or Loss is $totalWinOrLoss"
